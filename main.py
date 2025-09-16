0b4722b1279a37364a7e0c370b07d2f48ae8ff4e
ISTIFANO <elamiriamir7@gmail.com>
2025-09-16 10:52:39 +0200

    Add initial implementation for personalized presentation challenge

---
+++ main.py
@@ -0,0 +1,9 @@
+# 	Challenge 1 : Présentation personnalisée 
+
+get_prenom = input("donner moi votre nom")
+get_age = str(input("donner moi votre age "))
+
+print("votre age est " + get_age + " votre nom est " + get_prenom)
+
+
+
