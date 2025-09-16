e8f18e065529540e219c388a4c757adc05befd7b
ISTIFANO <elamiriamir7@gmail.com>
2025-09-16 16:55:56 +0200

    afficher chaque caractère un par un

--- Fonctions.py
+++ Fonctions.py
@@ -1,3 +1,4 @@
+import math
 #	Challenge 1 : Présentation personnalisé
 
 
@@ -21,3 +22,44 @@ def calculation(a, b):
     somme = a + b
     difference = a - b
     return somme, difference
+
+# Challenge 3 : Mini-projets algorithmiques regroupés
+
+# factorielle 
+
+def factorielle(nb):
+    result = 1
+    for i in range(1, nb + 1):
+        result *= i
+    return result
+
+# table de multiplication
+
+def TableMultiplication(m):
+    resultat = []
+    for i in range(1,10):
+     resultat.append(m*i);
+    return resultat
+
+print(TableMultiplication(5))
+
+# carré parfait.
+
+def perfectCube(N):
+    number = math.isqrt(N)
+    if(number**2 == N):
+        return 1
+    else :
+        return 0
+perfectCube(16)
+
+# afficher chaque caractère un par un.
+def affichageChar(char):
+ i=0
+ for i in range(0,len(char)):
+    print(char[i])
+
+affichageChar("dndks")
+
+
+
