5a90fa660ad05cfb803c4c5ecd8d3c189c878ec6
ISTIFANO <elamiriamir7@gmail.com>
2025-09-16 18:08:37 +0200

    Calculate and print the average of notes in Listes.py

--- Listes.py
+++ Listes.py
@@ -1,5 +1,12 @@
 # extrait toutes les notes supérieures à la moyenne et les stocke dans une nouvelle liste.
 notes = [12, 4, 14, 11, 18, 13, 7, 10, 5, 9, 15, 8, 14, 16]
 
-for i in range(notes):
-    print(notes[i])
+sum = 0
+nb = len(notes)
+for i in range(nb):
+    sum += notes[i]
+
+moy = sum / nb
+print(moy)
+
+# Challenge 2 : Les mots communs
