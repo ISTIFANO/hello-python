a792ea8e585357759ae04e655341a6c95c4c1ee8
ISTIFANO <elamiriamir7@gmail.com>
2025-09-16 13:26:31 +0200

    Refactor and expand horaire_sup function

--- Fonctions.py
+++ Fonctions.py
@@ -1,1 +1,16 @@
 #	Challenge 1 : Présentation personnalisé
+
+
+
+def horaire_sup(nom: str, salaire_horaire: float, nb_heures: int) -> float:
+    salaire_total = nb_heures * salaire_horaire
+
+    if nb_heures > 40:
+        heures_sup = nb_heures - 40
+        salaire_total = 40 * salaire_horaire + heures_sup * salaire_horaire * 1.5
+
+    return salaire_total
+
+
+horaire_sup("aamir",22.2,12)    
+
