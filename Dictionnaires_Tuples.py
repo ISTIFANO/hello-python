fa1cee2248e268528a2860b1ba6052715eb4ff10
ISTIFANO <elamiriamir7@gmail.com>
2025-09-17 12:47:31 +0200

    Update RAM value in First_dict to 32G and demonstrate two methods for modification

--- Dictionnaires_Tuples.py
+++ Dictionnaires_Tuples.py
@@ -1,1 +1,22 @@
+First_dict = { "Appareil": "Laptop", "Marque": "IBM",
+               "Carte mère": "MSI Z490",
+                 "Carte Graphique":"GeForce RTX 3070", 
+                 "RAM": "16G", "Processeur": "Intel core i7-G11",
+                   "SSD": "1 To" }
+
 # Challenge 1 : Manipulation des dictionnaires Python
+ 
+# first methode  
+
+First_dict["RAM"]="32G"
+
+# 2 methode 
+for cle, value in First_dict.items():
+    if cle == "RAM":
+        First_dict[cle] = "32G"
+
+print(First_dict)
+
+# Afficher successivement :
+
+
