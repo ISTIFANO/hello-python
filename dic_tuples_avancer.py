bcbda7009225c038e453751cb434975c95aee8a0
ISTIFANO <elamiriamir7@gmail.com>
2025-09-19 11:49:43 +0200

    [200~Challenge 2 : Transformation Avancée de Données

--- dic_tuples_avancer.py
+++ dic_tuples_avancer.py
@@ -12,3 +12,13 @@ print(filter_resultat)
 
 
 # Challenge 2 : Transformation Avancée de Données
+
+mixed_list = [2,3,-4,9,-6,8]
+
+get_number_positive = list(filter(lambda number : number>0,mixed_list))
+get_cub_nb = list(map(lambda number : number*number,get_number_positive))
+
+get_sorted_nb = list(sorted(get_cub_nb))
+print(get_sorted_nb)
+
+# Challenge 3 : Tri personnalisé avec clé dynamique
